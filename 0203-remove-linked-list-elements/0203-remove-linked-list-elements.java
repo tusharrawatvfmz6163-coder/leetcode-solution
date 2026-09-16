49d40896ae693e751e695dfbb0b803c0c487bf85
tusharrawatvfmz6163-coder <tusharrawatvfmz6163@gmail.com>
2026-09-16 16:24:23 +0530

    Time: 1 ms (96.7%), Space: 47.5 MB (7.85%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -1,18 +1,15 @@
  class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        
-        while (head != null && head.val == val) {
+        while (head != null && head.val == val)
             head = head.next;
-        }
 
         ListNode current = head;
 
         while (current != null && current.next != null) {
-            if (current.next.val == val) {
+            if (current.next.val == val)
                 current.next = current.next.next;
-            } else {
+            else
                 current = current.next;
-            }
         }
 
         return head;
