75d4863623a02a55b1d44d7e4ba30b865bd7dbda
tusharrawatvfmz6163-coder <tusharrawatvfmz6163@gmail.com>
2026-09-17 15:42:05 +0530

    Time: 1303 ms (5.1%), Space: 106.7 MB (5.32%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,17 @@
+ class Solution {
+    public boolean isPalindrome(ListNode head) {
+        String str = "";
+
+        for (ListNode temp = head; temp != null; temp = temp.next) {
+            str += temp.val;
+        }
+
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
