534cbaa3fcc37080ae8f9f44b1d1774e372b42d9
tusharrawatvfmz6163-coder <tusharrawatvfmz6163@gmail.com>
2026-09-22 16:43:25 +0530

    Time: 0 ms (100%), Space: 47.2 MB (89.19%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,40 @@
+ class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        // Search for the node
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } 
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } 
+        else {
+            // Node found
+
+            // Case 1: No left child
+            if (root.left == null) {
+                return root.right;
+            }
+
+            // Case 2: No right child
+            if (root.right == null) {
+                return root.left;
+            }
+
+            // Case 3: Two children
+            TreeNode successor = root.right;
+
+            while (successor.left != null) {
+                successor = successor.left;
+            }
+
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
+        }
+
+        return root;
+    }
+}
